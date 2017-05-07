fc210cfe640732ffd7ae8801d667fbd9d419e286
muphasa <ibalex.salino@gmail.com>
2017-05-07 06:52:00 +0200

    merde

--- group1000project/public/js/controllers.js
+++ group1000project/public/js/controllers.js
@@ -2,18 +2,18 @@ angular.module("app", [])
   .controller("head", function($scope){
     $scope.pageTitle = "Group1000";
   })
-  .controller("index", function($rootscope, $scope){
+  .controller("index", function($rootScope, $scope){
     $scope.playListTitle = "Coucou";
-    $rootscope.logged = false;
-    $scope.email = "";
-    $scope.pwd = "";
-    // $scope.auth = function() {
-    //   var email = "ibalex.salino@gmail.com";
-    //   var pwd = "coucou";
-    //   if (angular.equals($scope.email, email) && angular.equals($scope.pwd, pwd)){
-    //     $rootscope.logged = true;
-    //   }
-    // }
+    $rootScope.logged = false;
+    $scope.email = {};
+    $scope.pwd = {};
+    $scope.auth = function() {
+      var email = "ibalex.salino@gmail.com";
+      var pwd = "coucou";
+      //if (angular.equals($scope.email, email) && angular.equals($scope.pwd, pwd)){
+      //  $rootScope.logged = true;
+      //}
+    }
   })
 
   .controller("navbar", function($scope){
