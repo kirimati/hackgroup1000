56f2f568f171abcf7d2b77a619f47bd8ac803fcd
TiboDica <priou.thibaud@laposte.net>
2017-05-07 10:56:33 +0200

    unfinished

--- group1000project/public/js/controllers.js
+++ group1000project/public/js/controllers.js
@@ -1,11 +1,9 @@
 angular.module('app', ['services'])
 
-  .controller("head", function($scope) {
-    $scope.pageTitle = "Group1000";
-  })
-
   .controller("setCtrl", function($scope, Resources) {
     $scope.set = {};
+    $scope.set.picks = [];
+    $scope.set.links = [];
 
     $scope.addSet = function(username) {
       Resources.sets.save({username: username}, {set: $scope.set}, function() {
@@ -13,8 +11,4 @@ angular.module('app', ['services'])
             //$state.go("#", {}, {reload: true});
       });
     };
-  })
-
-  .controller("navbar", function($scope){
-    $scope.barTitle = "Group1000";
   });
